cba45876d01042b826c63e208aa446ea4a2dc38f
opeyf <llfs@phezzan.xyz>
2022-06-14 15:15:06 +0800

    replace discord url

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,7 +12,7 @@ import app_PHl_4 from "../../assets/images/app_PHl_icon4_1@2x.png"
 import Header from "./header"
 import styles from "./index.module.scss"
 
-const Discord = "https://discord.gg/nbVjsq57"
+const Discord = "https://discord.gg/SEmST5eZ"
 const Litepaper = "https://litepaper.phezzan.xyz"
 
 const Home = () => {
