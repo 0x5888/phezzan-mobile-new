929a884d0a98ee8c5abb042add9e15d93428a275
opeyf <llfs@phezzan.xyz>
2022-06-27 15:11:38 +0800

    discord url

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,7 +12,7 @@ import app_PHl_4 from "../../assets/images/app_PHl_icon4_1@2x.png"
 import Header from "./header"
 import styles from "./index.module.scss"
 
-const Discord = "https://discord.gg/JshsCYSe"
+const Discord = "https://discord.gg/MqypxPZP"
 const Litepaper = "https://litepaper.phezzan.xyz"
 
 const Home = () => {
