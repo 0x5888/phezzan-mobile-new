7a8bda6679765802db22d8da6351dfcad4f9ca39
opeyf <llfs@phezzan.xyz>
2022-10-04 21:58:26 +0800

    replace discord url

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,7 +12,7 @@ import app_PHl_4 from "../../assets/images/app_PHl_icon4_1@2x.png"
 import Header from "./header"
 import styles from "./index.module.scss"
 
-const Discord = "https://discord.gg/tq4mQTVM"
+const Discord = "https://discord.gg/VNzhkbQRAk"
 const Litepaper = " https://learn.phezzan.xyz"
 
 const Home = () => {
